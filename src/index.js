59b12ce9b9a387bf7f514acc3541a8060340224b
Mark Brooks <Markabrooks@gmail.com>
2018-07-17 22:36:01 -0600

    install react router and test successful

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Howdy extends React.Component {
+  render() {
+    return <div>Howdy</div>;
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return <div>Bye Bye Now!</div>;
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/howdy" component={Howdy} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
