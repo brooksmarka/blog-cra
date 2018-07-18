b8e2d91bdbee6644556717badbf3483001f09e90
Mark Brooks <Markabrooks@gmail.com>
2018-07-18 09:07:08 -0600

    root path to PostsIndex created

---
+++ src/components/posts_index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+//import PropTypes from "prop-types";
+
+class PostsIndex extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+  render() {
+    return <div className="PostsIndex">Posts Index</div>;
+  }
+}
+export default PostsIndex;
--- src/index.js
+++ src/index.js
@@ -4,29 +4,16 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import App from "./components/App";
 import reducers from "./reducers";
+import PostsIndex from "./components/posts_index";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Howdy extends React.Component {
-  render() {
-    return <div>Howdy</div>;
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Bye Bye Now!</div>;
-  }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/howdy" component={Howdy} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>,
