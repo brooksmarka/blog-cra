457f2d43774656910cb9601d1c232739315b4764
Mark Brooks <Markabrooks@gmail.com>
2018-07-19 12:32:49 -0600

    post title rendering on screen

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import _ from "lodash";
 //import PropTypes from "prop-types";
 
 class PostsIndex extends Component {
@@ -9,14 +10,34 @@ class PostsIndex extends Component {
   static defaultProps = {};
 
   componentDidMount() {
-    this.props.fetchPosts();
+    return this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
   }
 
   render() {
-    return <div className="PostsIndex">Posts Index</div>;
+    return (
+      <div className="PostsIndex">
+        <h3>posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostsIndex);
