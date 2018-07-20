c9914d27de6efdf016aa9da8e45cbd70f30a19df
Mark Brooks <Markabrooks@gmail.com>
2018-07-20 09:08:26 -0600

    Scaffold posts_new component and add route config

---
+++ src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class PostsNew extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+  render() {
+    return <div className="PostsNew">Posts New</div>;
+  }
+}
+export default PostsNew;
