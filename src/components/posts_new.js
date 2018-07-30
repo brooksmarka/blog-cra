7d771fc7d4ab332c4a5cd601b74d2e6dd2b5969b
Mark Brooks <Markabrooks@gmail.com>
2018-07-30 16:34:25 -0600

    navigation back to root route from new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
 class PostsNew extends Component {
   static propTypes = {};
@@ -42,6 +43,9 @@ class PostsNew extends Component {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link className="btn btn-danger" to="/">
+          Cancel
+        </Link>
       </form>
     );
   }
