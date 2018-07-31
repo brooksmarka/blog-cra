24dbe50c679e85c406d3dd69449b2ef403b80d5d
Mark Brooks <Markabrooks@gmail.com>
2018-07-31 11:43:15 -0600

    posts_show component created and route implemented

---
+++ src/components/posts_show.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+//import PropTypes from 'prop-types';
+
+class PostsShow extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+  render() {
+    return <div>Posts Show!</div>;
+  }
+}
+export default PostsShow;
